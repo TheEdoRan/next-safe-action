2aafda4affeb98329295cbb3504e1e746a92ec84
Edoardo Ranghieri <edoardo@ranghieri.com>
2023-06-05 18:12:52 +0200

    refactor: better argument and type names, add JSDoc comments

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,54 @@
 import type { z } from "zod";
 
-/*********************** TYPES ***********************/
+// ********************* TYPES *********************
 
-// The type for client action, which is called by components.
-// You pass the input data here, and it's all typesafe.
-export type ClientAction<IV extends z.ZodTypeAny, AO> = (input: z.input<IV>) => Promise<{
+/**
+ * The function called from Client Components with typesafe input data for the Server Action.
+ */
+export type ClientCaller<IV extends z.ZodTypeAny, AO> = (input: z.input<IV>) => Promise<{
 	data?: AO;
 	serverError?: true;
 	validationError?: Partial<Record<keyof z.input<IV>, string[]>>;
 }>;
 
-// We need to overload the `safeAction` function, because some actions
-// need authentication, and others don't, so you can pass the `withAuth: true` property
-// in the `opts` arg, to get back both `parsedInput` and `authArgs` in the server
-// action function definition.
-// `authArgs` comes from the previously defined `getAuthData` function.
-export type SafeActionOverload<AuthData extends object> = {
+/**
+ * Action initializer overload, because some actions need authentication and others don't.
+ * 1. If you don't need authentication, just pass `input` (Zod input validator) to `opts`.
+ * You'll receive the parsed input as Server Action function definition parameter.
+ * {@link https://github.com/theedoran/next-safe-action#1-the-direct-way See an example}.
+ * 2. If you need authentication, you have to pass `withAuth: true`,
+ * along with `input` (Zod input validator) when you're creating a new action.
+ * In this case, you receive both the parsed input and auth data as
+ * Server Action function definition parameters.
+ * {@link https://github.com/theedoran/next-safe-action#authenticated-action See an example}.
+ */
+export type ActionOverload<AuthData extends object> = {
 	<const IV extends z.ZodTypeAny, const AO>(
 		opts: {
 			input: IV;
 			withAuth?: false;
 		},
-		actionDefinition: (parsedInput: z.input<IV>, authArgs: undefined) => Promise<AO>
-	): ClientAction<IV, AO>;
+		actionDefinition: (parsedInput: z.input<IV>, authData: undefined) => Promise<AO>
+	): ClientCaller<IV, AO>;
 
 	<const IV extends z.ZodTypeAny, const AO>(
 		opts: {
 			input: IV;
 			withAuth: true;
 		},
-		actionDefinition: (parsedInput: z.input<IV>, authArgs: AuthData) => Promise<AO>
-	): ClientAction<IV, AO>;
+		actionDefinition: (parsedInput: z.input<IV>, authData: AuthData) => Promise<AO>
+	): ClientCaller<IV, AO>;
 };
 
-/*********************** FUNCTIONS ***********************/
+// ********************* FUNCTIONS *********************
 
-// This is the safe action initializer.
+/**
+ * This is the safe action initializer.
+ * @param createOpts Options for creating a new action client.
+ * @returns {Function} A function that creates a new action, to be used in server files.
+ *
+ * {@link https://github.com/theedoran/next-safe-action#project-configuration See an example}
+ */
 export const createSafeActionClient = <AuthData extends object>(createOpts?: {
 	serverErrorLogFunction?: (e: any) => void | Promise<void>;
 	getAuthData?: () => Promise<AuthData>;
@@ -50,12 +63,12 @@ export const createSafeActionClient = <AuthData extends object>(createOpts?: {
 			console.log("Action error:", errMessage);
 		});
 
-	// `safeAction` is the server function that creates a new action.
+	// `action` is the server function that creates a new action.
 	// It expects an input validator, a optional `withAuth` property, and a
 	// definition function, so the action knows what to do on the server when
 	// called by the client.
 	// It returns a function callable by the client.
-	const safeAction: SafeActionOverload<AuthData> = (opts, actionDefinition) => {
+	const action: ActionOverload<AuthData> = (opts, actionDefinition) => {
 		// This is the function called by client. If `input` fails the validator
 		// parsing, the function will return a `validationError` object, containing
 		// all the invalid fields provided.
@@ -94,5 +107,5 @@ export const createSafeActionClient = <AuthData extends object>(createOpts?: {
 		};
 	};
 
-	return safeAction;
+	return action;
 };
