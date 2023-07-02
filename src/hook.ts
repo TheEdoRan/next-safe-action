f9484b5d194ca3d99b098aec90ddf10d2c148af5
Edoardo Ranghieri <edoardo@ranghieri.com>
2023-07-02 22:53:06 +0200

    refactor: `AO` -> `Data`

--- src/hook.ts
+++ src/hook.ts
@@ -15,7 +15,7 @@ import type { ClientCaller } from ".";
 /**
  * Type of `res` object returned by `useAction` and `useOptimisticAction` hooks.
  */
-export type HookRes<IV extends z.ZodTypeAny, AO> = Awaited<ReturnType<ClientCaller<IV, AO>>> & {
+export type HookRes<IV extends z.ZodTypeAny, Data> = Awaited<ReturnType<ClientCaller<IV, Data>>> & {
 	fetchError?: unknown;
 };
 
@@ -23,14 +23,17 @@ export type HookRes<IV extends z.ZodTypeAny, AO> = Awaited<ReturnType<ClientCall
  * Type of hooks callbacks (`onSuccess` and `onError`).
  * These are executed when the action succeeds or fails.
  */
-export type HookCallbacks<IV extends z.ZodTypeAny, AO> = {
-	onSuccess?: (data: NonNullable<Pick<HookRes<IV, AO>, "data">["data"]>, reset: () => void) => void;
-	onError?: (error: Omit<HookRes<IV, AO>, "data">, reset: () => void) => void;
+export type HookCallbacks<IV extends z.ZodTypeAny, Data> = {
+	onSuccess?: (
+		data: NonNullable<Pick<HookRes<IV, Data>, "data">["data"]>,
+		reset: () => void
+	) => void;
+	onError?: (error: Omit<HookRes<IV, Data>, "data">, reset: () => void) => void;
 };
 
 // ********************* FUNCTIONS *********************
 
-const getActionStatus = <IV extends z.ZodTypeAny, AO>(res: HookRes<IV, AO>) => {
+const getActionStatus = <const IV extends z.ZodTypeAny, const Data>(res: HookRes<IV, Data>) => {
 	const hasSucceded = typeof res.data !== "undefined";
 	const hasErrored =
 		typeof res.validationError !== "undefined" ||
@@ -42,12 +45,12 @@ const getActionStatus = <IV extends z.ZodTypeAny, AO>(res: HookRes<IV, AO>) => {
 	return { hasExecuted, hasSucceded, hasErrored };
 };
 
-const useActionCallbacks = <const IV extends z.ZodTypeAny, const AO>(
-	res: HookRes<IV, AO>,
+const useActionCallbacks = <const IV extends z.ZodTypeAny, const Data>(
+	res: HookRes<IV, Data>,
 	hasSucceded: boolean,
 	hasErrored: boolean,
 	reset: () => void,
-	cb?: HookCallbacks<IV, AO>
+	cb?: HookCallbacks<IV, Data>
 ) => {
 	const onSuccessRef = useRef(cb?.onSuccess);
 	const onErrorRef = useRef(cb?.onError);
@@ -72,15 +75,15 @@ const useActionCallbacks = <const IV extends z.ZodTypeAny, const AO>(
  *
  * {@link https://github.com/theedoran/next-safe-action#2-the-hook-way See an example}
  */
-export const useAction = <const IV extends z.ZodTypeAny, const AO>(
-	clientCaller: ClientCaller<IV, AO>,
-	cb?: HookCallbacks<IV, AO>
+export const useAction = <const IV extends z.ZodTypeAny, const Data>(
+	clientCaller: ClientCaller<IV, Data>,
+	cb?: HookCallbacks<IV, Data>
 ) => {
 	const [isExecuting, startTransition] = useTransition();
 	const executor = useRef(clientCaller);
-	const [res, setRes] = useState<HookRes<IV, AO>>({});
+	const [res, setRes] = useState<HookRes<IV, Data>>({});
 
-	const { hasExecuted, hasSucceded, hasErrored } = getActionStatus<IV, AO>(res);
+	const { hasExecuted, hasSucceded, hasErrored } = getActionStatus<IV, Data>(res);
 
 	const execute = useCallback(async (input: z.input<IV>) => {
 		startTransition(() => {
@@ -120,10 +123,14 @@ export const useAction = <const IV extends z.ZodTypeAny, const AO>(
  *
  * {@link https://github.com/theedoran/next-safe-action#optimistic-update--experimental See an example}
  */
-export const useOptimisticAction = <const IV extends z.ZodTypeAny, const AO, State extends object>(
-	clientCaller: ClientCaller<IV, AO>,
+export const useOptimisticAction = <
+	const IV extends z.ZodTypeAny,
+	const Data,
+	State extends object
+>(
+	clientCaller: ClientCaller<IV, Data>,
 	defaultOptState: State,
-	cb?: HookCallbacks<IV, AO>
+	cb?: HookCallbacks<IV, Data>
 ) => {
 	const [optState, syncState] = experimental_useOptimistic<
 		State & { __isExecuting__: boolean },
@@ -135,9 +142,9 @@ export const useOptimisticAction = <const IV extends z.ZodTypeAny, const AO, Sta
 	}));
 
 	const executor = useRef(clientCaller);
-	const [res, setRes] = useState<HookRes<IV, AO>>({});
+	const [res, setRes] = useState<HookRes<IV, Data>>({});
 
-	const { hasExecuted, hasSucceded, hasErrored } = getActionStatus<IV, AO>(res);
+	const { hasExecuted, hasSucceded, hasErrored } = getActionStatus<IV, Data>(res);
 
 	const execute = useCallback(
 		(input: z.input<IV>, newServerState: Partial<State>) => {
