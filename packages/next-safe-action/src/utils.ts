f8123737a25e3398359f1171449075e5dbcc01c9
Grant Burry <grant@burry.me>
2023-10-10 11:53:44 -0400

    fix: use same error type guards as Next internals

--- packages/next-safe-action/src/utils.ts
+++ packages/next-safe-action/src/utils.ts
@@ -1,4 +1,24 @@
-export const isError = (e: any) => e instanceof Error;
-export const isNextRedirectError = (e: any) => isError(e) && e.message === "NEXT_REDIRECT";
-export const isNextNotFoundError = (e: any) => isError(e) && e.message === "NEXT_NOT_FOUND";
+import type { RedirectError } from "next/dist/client/components/redirect";
+
 export const DEFAULT_SERVER_ERROR = "Something went wrong while executing the operation";
+
+const REDIRECT_ERROR_CODE = "NEXT_REDIRECT";
+const NOT_FOUND_ERROR_CODE = "NEXT_NOT_FOUND";
+
+export const isNextRedirectError = <U extends string>(error: any): error is RedirectError<U> => {
+	if (typeof (error == null ? void 0 : error.digest) !== "string") return false;
+	const [errorCode, type, destination, permanent] = error.digest.split(";", 4);
+	return (
+		errorCode === REDIRECT_ERROR_CODE &&
+		(type === "replace" || type === "push") &&
+		typeof destination === "string" &&
+		(permanent === "true" || permanent === "false")
+	);
+};
+
+type NotFoundError = Error & { digest: typeof NOT_FOUND_ERROR_CODE };
+
+export const isNextNotFoundError = (error: any): error is NotFoundError =>
+	(error == null ? void 0 : error.digest) === NOT_FOUND_ERROR_CODE;
+
+export const isError = (error: any): error is Error => error instanceof Error;
