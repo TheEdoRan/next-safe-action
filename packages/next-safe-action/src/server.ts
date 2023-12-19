29a8edfaf58dbdc4ecb4456b06c6e578671a2a58
Edoardo Ranghieri <edoardo@ranghieri.com>
2023-12-19 02:40:21 +0100

    fix: add .js file extension for imported files

--- packages/next-safe-action/src/server.ts
+++ packages/next-safe-action/src/server.ts
@@ -1,5 +1,5 @@
-import { isNotFoundError } from "next/dist/client/components/not-found";
-import { isRedirectError } from "next/dist/client/components/redirect";
+import { isNotFoundError } from "next/dist/client/components/not-found.js";
+import { isRedirectError } from "next/dist/client/components/redirect.js";
 import type { z } from "zod";
 import type { SafeAction, ServerCodeFn } from "./types";
 import type { MaybePromise } from "./utils";
