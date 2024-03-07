45ec06b4fb92081cd8b092db83f14d7ef62b84d0
Edoardo Ranghieri <edoardo@ranghieri.com>
2024-03-07 14:13:28 +0100

    refactor(example): add temporary client form example

--- packages/example-app/src/app/(examples)/client-form/signup-action.ts
+++ packages/example-app/src/app/(examples)/client-form/signup-action.ts
@@ -1,17 +1,12 @@
 "use server";
 
-import { action } from "@/lib/safe-action";
-import z from "zod";
-import { zfd } from "zod-form-data";
+type PrevState = {
+	message: string;
+};
 
-const schema = zfd.formData({
-	email: zfd.text(z.string().email()),
-	password: zfd.text(z.string().min(8)),
-});
-
-export const signup = action(schema, async ({ email, password }) => {
-	console.log("Email:", email, "Password:", password);
+// Temporary implementation.
+export const signupAction = (prevState: PrevState, formData: FormData) => {
 	return {
-		success: true,
+		message: "Logged in successfully!",
 	};
-});
+};
