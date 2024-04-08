d53227e5597b8b58afddd96cf5fb4603abe27c51
Edoardo Ranghieri <edoardo@ranghieri.com>
2024-04-08 22:13:35 +0200

    docs(website): format stars count

--- website/src/components/landing/stars-button.tsx
+++ website/src/components/landing/stars-button.tsx
@@ -29,7 +29,12 @@ export function StarsButton() {
 			rel="noopener noreferrer"
 			className="!no-underline hover:!bg-sky-100 transition !text-zinc-900 cursor-pointer rounded-lg mr-4 bg-zinc-50 px-3 py-2 font-bold inline-flex items-center justify-center space-x-1 text-sm sm:text-lg md:text-xl">
 			<Star className="w-4 h-4 sm:w-6 sm:h-6" />
-			<span>{starsCount ? starsCount : ". . ."} GitHub stars</span>
+			<span>
+				{starsCount
+					? Intl.NumberFormat("en", { notation: "compact" }).format(starsCount)
+					: ". . ."}{" "}
+				GitHub stars
+			</span>
 		</a>
 	);
 }
