d784c38d83bf4550756d9137ace211b1b238c874
Edoardo Ranghieri <edoardo@ranghieri.com>
2024-04-24 00:23:59 +0200

    chore(example): add GitHub link for examples

--- apps/basic-example/src/app/(examples)/layout.tsx
+++ apps/basic-example/src/app/(examples)/layout.tsx
@@ -1,16 +1,20 @@
 import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 import { type ReactNode } from "react";
+import { ExampleGithubLink } from "../_components/example-github-link";
 
 export default function ExamplesLayout({ children }: { children: ReactNode }) {
 	return (
 		<div>
-			<Link
-				href="/"
-				className="text-center flex items-center justify-center text-blue-600 dark:text-blue-400 hover:underline w-fit mx-auto">
-				<ChevronLeft className="w-6 h-6" />
-				<span className="text-lg font-semibold tracking-tight">Go back</span>
-			</Link>
+			<div className="flex space-x-10 items-center justify-center">
+				<Link
+					href="/"
+					className="flex items-center text-blue-600 dark:text-blue-400 hover:underline">
+					<ChevronLeft className="w-6 h-6" />
+					<span className="text-lg font-semibold tracking-tight">Go back</span>
+				</Link>
+				<ExampleGithubLink className="text-lg font-semibold flex items-center justify-center space-x-2 text-blue-600 dark:text-blue-400 hover:underline" />
+			</div>
 			<div className="mt-4">{children}</div>
 		</div>
 	);
