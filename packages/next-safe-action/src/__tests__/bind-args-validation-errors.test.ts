f93c55d660bdd9e1978a4601eb4fbe8d9e01cab2
Edoardo Ranghieri <edoardo@ranghieri.com>
2024-06-06 01:53:01 +0200

    test: add some more bind args validation errors tests

--- packages/next-safe-action/src/__tests__/bind-args-validation-errors.test.ts
+++ packages/next-safe-action/src/__tests__/bind-args-validation-errors.test.ts
@@ -3,7 +3,7 @@
 import assert from "node:assert";
 import { test } from "node:test";
 import { z } from "zod";
-import { createSafeActionClient } from "..";
+import { createSafeActionClient, flattenBindArgsValidationErrors, formatBindArgsValidationErrors } from "..";
 
 // Default client tests.
 
@@ -35,6 +35,47 @@ test("action with invalid bind args input gives back an object with correct `bin
 			{
 				id: {
 					_errors: ["Invalid uuid"],
+				},
+			},
+		],
+	};
+
+	assert.deepStrictEqual(actualResult, expectedResult);
+});
+
+test("action with invalid bind args input gives back an object with correct `bindArgsValidationErrors` (default formatted shape overridden by custom flattened shape)", async () => {
+	const bindArgsSchemas: [age: z.ZodNumber, userId: z.ZodString, product: z.ZodObject<{ id: z.ZodString }>] = [
+		z.number().positive(),
+		z.string().uuid(),
+		z.object({
+			id: z.string().uuid(),
+		}),
+	];
+
+	const action = dac
+		.bindArgsSchemas(bindArgsSchemas, { handleBindArgsValidationErrorsShape: flattenBindArgsValidationErrors })
+		.action(async () => {
+			return {
+				ok: true,
+			};
+		});
+
+	const actualResult = await action(-123, crypto.randomUUID(), { id: "invalid_uuid" });
+
+	const expectedResult = {
+		bindArgsValidationErrors: [
+			{
+				formErrors: ["Number must be greater than 0"],
+				fieldErrors: {},
+			},
+			{
+				formErrors: [],
+				fieldErrors: {},
+			},
+			{
+				formErrors: [],
+				fieldErrors: {
+					id: ["Invalid uuid"],
 				},
 			},
 		],
@@ -83,6 +124,47 @@ test("action with invalid bind args input gives back an object with correct `bin
 	assert.deepStrictEqual(actualResult, expectedResult);
 });
 
+test("action with invalid bind args input gives back an object with correct `bindArgsValidationErrors` (set formatted shape overridden by custom flattened shape)", async () => {
+	const bindArgsSchemas: [age: z.ZodNumber, userId: z.ZodString, product: z.ZodObject<{ id: z.ZodString }>] = [
+		z.number().positive(),
+		z.string().uuid(),
+		z.object({
+			id: z.string().uuid(),
+		}),
+	];
+
+	const action = foac
+		.bindArgsSchemas(bindArgsSchemas, { handleBindArgsValidationErrorsShape: flattenBindArgsValidationErrors })
+		.action(async () => {
+			return {
+				ok: true,
+			};
+		});
+
+	const actualResult = await action(-123, crypto.randomUUID(), { id: "invalid_uuid" });
+
+	const expectedResult = {
+		bindArgsValidationErrors: [
+			{
+				formErrors: ["Number must be greater than 0"],
+				fieldErrors: {},
+			},
+			{
+				formErrors: [],
+				fieldErrors: {},
+			},
+			{
+				formErrors: [],
+				fieldErrors: {
+					id: ["Invalid uuid"],
+				},
+			},
+		],
+	};
+
+	assert.deepStrictEqual(actualResult, expectedResult);
+});
+
 // Flattened shape tests.
 
 const flac = createSafeActionClient({
@@ -120,6 +202,42 @@ test("action with invalid bind args input gives back an object with correct `bin
 				formErrors: [],
 				fieldErrors: {
 					id: ["Invalid uuid"],
+				},
+			},
+		],
+	};
+
+	assert.deepStrictEqual(actualResult, expectedResult);
+});
+
+test("action with invalid bind args input gives back an object with correct `bindArgsValidationErrors` (set flattened shape overridden by custom formatted shape)", async () => {
+	const bindArgsSchemas: [age: z.ZodNumber, userId: z.ZodString, product: z.ZodObject<{ id: z.ZodString }>] = [
+		z.number().positive(),
+		z.string().uuid(),
+		z.object({
+			id: z.string().uuid(),
+		}),
+	];
+
+	const action = flac
+		.bindArgsSchemas(bindArgsSchemas, { handleBindArgsValidationErrorsShape: formatBindArgsValidationErrors })
+		.action(async () => {
+			return {
+				ok: true,
+			};
+		});
+
+	const actualResult = await action(-123, crypto.randomUUID(), { id: "invalid_uuid" });
+
+	const expectedResult = {
+		bindArgsValidationErrors: [
+			{
+				_errors: ["Number must be greater than 0"],
+			},
+			{},
+			{
+				id: {
+					_errors: ["Invalid uuid"],
 				},
 			},
 		],
