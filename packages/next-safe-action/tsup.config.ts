6d6ec34ead36bc1e05f8f458a4e58c44c7c6dc91
Edoardo Ranghieri <edoardo@ranghieri.com>
2024-07-16 01:43:54 +0200

    build: don't bundle files

--- packages/next-safe-action/tsup.config.ts
+++ packages/next-safe-action/tsup.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-	entry: ["src/index.ts", "src/hooks.ts", "src/stateful-hooks.ts"],
+	entry: ["src/*.ts"],
+	bundle: false,
 	format: ["esm"],
 	clean: true,
 	splitting: false,
