3c18fd30d99733b99a3bfbf6e92909635eb3df22
Edoardo Ranghieri <edoardo@ranghieri.com>
2024-07-19 19:13:24 +0200

    feat(validation): add Yup adapter

---
+++ packages/next-safe-action/src/adapters/yup.ts
@@ -0,0 +1,38 @@
+// https://github.com/decs/typeschema/blob/main/packages/yup/src/validation.ts
+
+import type { Schema as YupSchema } from "yup";
+import { ValidationError } from "yup";
+import type { IfInstalled, Infer, ValidationAdapter, ValidationIssue } from "../adapters.types";
+
+class YupAdapter implements ValidationAdapter {
+	async validate<S extends IfInstalled<YupSchema>>(schema: S, data: unknown) {
+		try {
+			const result = await schema.validate(data, { strict: true });
+
+			return {
+				success: true,
+				data: result as Infer<S>,
+			} as const;
+		} catch (e) {
+			if (e instanceof ValidationError) {
+				const { message, path } = e;
+
+				return {
+					success: false,
+					issues: [
+						{
+							message,
+							path: path && path.length > 0 ? path : undefined,
+						},
+					] as ValidationIssue[],
+				} as const;
+			}
+
+			throw e;
+		}
+	}
+}
+
+export function yupAdapter() {
+	return new YupAdapter();
+}
