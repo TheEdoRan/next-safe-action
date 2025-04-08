dead2f38ffbae9740c8a043ff6846442c52f3351
Edoardo Ranghieri <edoardo@ranghieri.com>
2025-04-08 12:06:12 +0200

    chore(website): update production url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -7,7 +7,7 @@ export default {
 	favicon: "img/favicon.ico",
 
 	// Set the production url of your site here
-	url: "https://next-safe-action.dev",
+	url: "https://v7.next-safe-action.dev",
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: "/",
